ec1f9017cbecf59510b11d9494128851de8f1aa8
JeremySeq <jaysequeir@gmail.com>
2026-07-13 12:32:59 -0500

    remove rolling hotbar 2 from enum

--- src/main/java/com/synapse/betterhotbars/hotbars/AnimatedHotbarTypes.java
+++ src/main/java/com/synapse/betterhotbars/hotbars/AnimatedHotbarTypes.java
@@ -3,14 +3,12 @@ package com.synapse.betterhotbars.hotbars;
 import com.synapse.betterhotbars.OverlayOption;
 import com.synapse.betterhotbars.hotbars.hotbar_types.RadialHotbar;
 import com.synapse.betterhotbars.hotbars.hotbar_types.RollingHotbar;
-import com.synapse.betterhotbars.hotbars.hotbar_types.RollingHotbar2;
 import com.synapse.betterhotbars.hotbars.hotbar_types.SmoothDefaultHotbar;
 
 public enum AnimatedHotbarTypes {
 
     RADIAL_HOTBAR(new RadialHotbar()),
     ROLLING_HOTBAR(new RollingHotbar()),
-    ROLLING_HOTBAR_2(new RollingHotbar2()),
     SMOOTH_DEFAULT_HOTBAR(new SmoothDefaultHotbar());
 
     private final OverlayOption hotbar;
